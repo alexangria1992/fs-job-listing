d6a4d84097d11f92efd3e3e588f55b096e6637a7
Alexandre Angrignon <alex040892@gmail.com>
2024-09-26 23:12:21 +1200

    create and display views

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return view("jobs.index");
+})->name('jobs');
+
+Route::get('/jobs/create', function () {
+    return view("jobs.create");
+})->name('jobs.create');
+
+// Route::get('/posts/{id}', function(string $id){
+//     return 'Post ' . $id;
+// });
+
+// Route::get('/posts/{id}/comments/{commentId}', function(string $id, string $commentId){
+//     return 'Post ' . $id . 'Comment ' . $commentId;
+// });
