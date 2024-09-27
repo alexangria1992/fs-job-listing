19794b0f50622bd3e9fed9ccfabe6e562d675c05
Alexandre Angrignon <alex040892@gmail.com>
2024-09-27 23:32:28 +1200

    type hinting in controllers

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+
 
 class JobController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         $jobs = [
             'Web Developer',
@@ -23,7 +25,7 @@ class JobController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
         return  view('jobs.create');
     }
@@ -31,15 +33,15 @@ class JobController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): string
     {
-        
+        return 'Store';
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): string
     {
         return 'show';
     }
@@ -47,25 +49,25 @@ class JobController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id): string
     {
-        //
+        return "Edit";
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): string
     {
-        //
+        return 'Update';
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): string
     {
-        //
+        return 'Destroy';
     }
 
 }
