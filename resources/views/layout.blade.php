e9ce19f6d68526aa3d5a0591a52b2d67c7102133
Alexandre Angrignon <alex040892@gmail.com>
2024-09-28 22:31:57 +1200

    Header Component & Url helper

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
       @vite('resources/css/app.css')
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>{{$title ?? 'Workopia | Find and List Jobs ' }}</title>
     @vite('resources/js/app.js')
 
