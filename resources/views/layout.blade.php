d714ad505de0107649125255c3c06aa97d39b040
Alexandre Angrignon <alex040892@gmail.com>
2024-10-10 11:44:18 +1300

    Hero Component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,13 +9,16 @@
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>{{ $title ?? 'Workopia | Find and List Jobs ' }}</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @vite('resources/js/app.js')
 
 </head>
 
 <body class="bg-gray-100">
     <x-header />
-
+    @if (request()->is('/'))
+        <x-hero />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{ $slot }}
     </main>
