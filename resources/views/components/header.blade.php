1547d7e0de52fad69e6db0b266357b231929f56d
Alexandre Angrignon <alex040892@gmail.com>
2024-10-10 10:40:13 +1300

    Mobile Menu Nav Link

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,3 @@
-{{-- 
-@php 
-    $isActive = request()->is('jobs');
-    echo $isActive ? "active" : "";
-@endphp --}}
 <header class="bg-blue-900 text-white p-4">
     <div class="container mx-auto flex justify-between items-center">
         <h1 class="text-3xl font-semibold">
@@ -35,15 +30,16 @@
         </button>
     </div>
     <nav id="mobile-menu" class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
-        <a href="{{ url('/jobs') }}" class="block px-4 py-2 hover:bg-blue-700">All Jobs</a>
-        <a href="{{ url('/jobs/saved') }}" class="block px-4 py-2 hover:bg-blue-700">Saved Jobs</a>
-        <a href="{{ url('/login') }}" class="block px-4 py-2 hover:bg-blue-700">Login</a>
-        <a href="{{ url('register') }}" class="block px-4 py-2 hover:bg-blue-700">Register</a>
-        <a href="{{ url('/dashboard.html') }}" class="block text-white hover:underline py-2">
-            <i class="fa fa-gauge mr-1"></i> Dashboard
-        </a>
-        <a href="{{ url('/jobs/create') }}" class="block px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black">
-            <i class="fa fa-edit"></i> Create Job
-        </a>
+        <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">
+            All Jobs
+        </x-nav-link>
+        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
+        <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">Login</x-nav-link>
+        <x-nav-link url="/register" :active="request()->is('register')" :mobile="true">Register</x-nav-link>
+        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" :mobile="true">Dashboard</x-nav-link>
+
+
+        <x-button-link url="/jobs/create" icon="edit" :block="true">Create
+            Job</x-button-link>
     </nav>
 </header>
