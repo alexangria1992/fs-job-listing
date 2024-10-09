ac68c94e141be9ecea89849bd9246d5e309fda13
Alexandre Angrignon <alex040892@gmail.com>
2024-10-10 11:02:56 +1300

    mobile menu toggle

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      @vite('resources/css/app.css')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>{{$title ?? 'Workopia | Find and List Jobs ' }}</title>
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>{{ $title ?? 'Workopia | Find and List Jobs ' }}</title>
     @vite('resources/js/app.js')
 
 </head>
+
 <body class="bg-gray-100">
-    <x-header/>
-    
-        <main class="container mx-auto p-4 mt-4">
-           {{$slot}}
-        </main>
+    <x-header />
+
+    <main class="container mx-auto p-4 mt-4">
+        {{ $slot }}
+    </main>
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
+
 </html>
