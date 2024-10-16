dd48da6156076f8299671f8f967cb8fe2ecb456c
Alexandre Angrignon <alex040892@gmail.com>
2024-10-17 11:15:16 +1300

    Eloquent Relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -31,4 +32,10 @@ class Job extends Model
         'company_website',
         'user_id'
     ];
+
+    //Relation to user
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2024_10_16_214731_add_fields_to_job_listings_table.php
+++ database/migrations/2024_10_16_214731_add_fields_to_job_listings_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,6 +15,8 @@ return new class extends Migration
         // Clear table data 
         DB::table('job_listings')->truncate();
         Schema::table('job_listings', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->after('id');
+
             $table->integer('salary');
             $table->string('tags')->nullable();
             $table->enum('job_type', ['Full-Time', 'Part-Time', 'Contract', 'Temporary', 'Internship', 'Volunteer', 'On-Call'])->default('Full-Time');
@@ -30,6 +33,9 @@ return new class extends Migration
             $table->text('company_description')->nullable();
             $table->string('company_logo')->nullable();
             $table->string('company_website')->nullable();
+
+            // Add user foriegn key constraint
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -39,7 +45,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('job_listings', function (Blueprint $table) {
-
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
             $table->dropColumn(['salary', 'tags', 'job_type', 'remote', 'requirements', 'benefits', 'address', 'city', 'state', 'zipcode', 'contact_email', 'contact_phone', 'company_name', 'company_description', 'company_logo', 'company_website']);
         });
     }
